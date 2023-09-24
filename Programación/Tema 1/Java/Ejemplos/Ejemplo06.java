90617b479c3e4b53f0b2ecae374eec987390c9ff
JBibu <javiims03alt@gmail.com>
2023-09-24 21:40:03 +0200

    renamed:    "Entornos de desarrollo/Pr\303\241ctica 1.1.pdf" -> "Entornos de desarrollo/Tema 1/Pr\303\241ctica 1.1.pdf" 	renamed:    "Entornos de desarrollo/Pr\303\241ctica 1.2.pdf" -> "Entornos de desarrollo/Tema 1/Pr\303\241ctica 1.2.pdf" 	new file:   "Programaci\303\263n/Tema 1/Java/Ejemplos/Ejemplo06.java" 	new file:   "Sistemas Inform\303\241ticos/Tema 1/Pr\303\241ctica 1.1.pdf" 	new file:   "Sistemas Inform\303\241ticos/Tema 1/Pr\303\241ctica 1.2.pdf"

---
+++ Programación/Tema 1/Java/Ejemplos/Ejemplo06.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ejemplo06 {
+    
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Introduce un número: ");
+        int numero = teclado.nextInt();
+        
+        if (numero % 2 == 0) {
+            System.out.println("El número es par");
+        }
+
+        teclado.close();
+        
+    }
+}
